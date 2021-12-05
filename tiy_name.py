939cf97fb46f1dca7cee06a128ec182d748009f5
soumouni <souad.mimouni13@gmail.com>
2021-12-05 22:47:24 +0100

    displayed name variable with a message

---
+++ tiy_name.py
@@ -0,0 +1,8 @@
+name= "jean baptist"
+print(f"Hello {name.title()}, would you like to learn some python?")
+
+print(f"\n {name.title()}")
+
+print(f"\n\t {name.upper()}")
+
+print (f" \n\t\t {name.lower()}")
