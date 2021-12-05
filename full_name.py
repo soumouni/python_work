8f905994b236c68f2c2c5a7db1d9f019f2a9385d
soumouni <souad.mimouni13@gmail.com>
2021-12-05 11:47:02 +0100

    worked with strings, strip, change var value, add tab, add new line, f-string

---
+++ full_name.py
@@ -0,0 +1,17 @@
+first_name= '  souad   '
+last_name='mimouni  '
+full_name=f"{first_name} {last_name}"
+print(full_name)
+
+first_name=first_name.strip()
+last_name=last_name.strip()
+full_name=f"{first_name} {last_name}"
+print(full_name)
+print()
+print(f"\tI am {full_name}")
+print (f"\n\t\tHello, {full_name} !")
+print (f"Hello, {full_name.title()} !!")
+print (f"Hello, {first_name.title()} {last_name.upper()} !!!")
+
+print("\nI am \n\tfrom \n\t\tAlgeria")
+
