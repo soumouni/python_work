de183782cdabfd5c185cc619fed4853517c21b90
soumouni <souad.mimouni13@gmail.com>
2021-12-06 14:28:51 +0100

    use simple list and print each element of it

---
+++ tiy_lists.py
@@ -0,0 +1,14 @@
+# try it yourself page 36
+#Lists
+friends=["Amel", "Lamia", "Ali", "Faycal"]
+print("The list of friends I named are:")
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[3])
+#printing a message for each friend
+print()
+print(f"Hello {friends[0]}")
+print(f"Bonjour {friends[1]}")
+print(f"Sabah el kheir {friends[2]}")
+print(f"Coucou {friends[3]}")
