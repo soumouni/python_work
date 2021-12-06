ae9a21668d4abc7953a4c971fb1964539dfeadda
soumouni <souad.mimouni13@gmail.com>
2021-12-06 11:17:42 +0100

    use numbers to make simple calculations, print results, use variables and comments

---
+++ tiy_numbers.py
@@ -0,0 +1,24 @@
+#try it yourself page 29
+#basic assignement
+print(5+3)
+print(9-1)
+print(16/2)
+print(4*2)
+print(2**3)
+
+#using variables
+ADD=5+3
+SUB=9-1
+DIV=16/2
+MUL=4*2
+POW=2**3
+
+print(f"\n5+3= {ADD}")
+print(f"9-1= {SUB}")
+print(f"16/2= {DIV}")
+print(f"4*2= {MUL}")
+print(f"2**3= {POW}")
+
+#favorite number
+FAV=15
+print(f"\nI don't really have a favorite number, but let's say it is {FAV}\n")
