5a5b0178c2bf25ee742e263a73784ccb240bd81a
soumouni <souad.mimouni13@gmail.com>
2021-12-08 23:07:11 +0100

    create tuple, print it, change it

---
+++ tiy_buffet.py
@@ -0,0 +1,11 @@
+# try it yourself page 68
+#buffet
+buffet=("salade","soupe","frittes", "poulet","flan")
+print("the restaurant offers a fixed buffet, that consist of:")
+for meal in buffet:
+    print(meal)
+
+buffet=("salade", "frittes", "chorba", "bourek")
+print("the restaurant changed its menu to the following:")
+for meal in buffet:
+    print(meal)
