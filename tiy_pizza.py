a1ca40ac33cceeb19d6e7a30a3426887fcde0e79
soumouni <souad.mimouni13@gmail.com>
2021-12-08 13:02:14 +0100

    create list, use for loop in list, end for loop

---
+++ tiy_pizza.py
@@ -0,0 +1,17 @@
+#try it yourself page 56
+#pizzas
+
+pizzas=["saumon", "creme fraiche" , "tuna"]
+print("\nmy favorite pizzas are:")
+for pizza in pizzas:
+    print(f"I like {pizza.title()} pizza")      #end of for loop
+print("I really like pizza, but kowing it is not very healthy, I rarely eat pizza")
+
+#animals
+animals=["dog", "cat", "chicken"]
+print("\nin a farm you can find")
+for animal in animals:
+    print(animal)
+print(f"\n{animals[0]} and {animals[1]} would make great pets")
+print(f"{animals[2]} gives eggs")
+print("\nany of these animals are a must in a farm\n")
