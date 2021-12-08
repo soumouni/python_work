4d1fd78784176b4e6ead12d47fb8e9366a6d02e5
soumouni <souad.mimouni13@gmail.com>
2021-12-08 22:40:58 +0100

    copy list, add elements to each list, print

--- tiy_pizza.py
+++ tiy_pizza.py
@@ -7,6 +7,20 @@ for pizza in pizzas:
     print(f"I like {pizza.title()} pizza")      #end of for loop
 print("I really like pizza, but kowing it is not very healthy, I rarely eat pizza")
 
+# try it yourself page 65
+friend_pizzas=pizzas[:]
+pizzas.append("carre")
+friend_pizzas.append("fumato")
+print("my favorite pizzas are")
+for pizza in pizzas:
+    print (pizza)
+print("\n my friend's favorite pizzas are:")
+for pizza in friend_pizzas:
+    print(pizza)
+
+
+
+
 #animals
 animals=["dog", "cat", "chicken"]
 print("\nin a farm you can find")
