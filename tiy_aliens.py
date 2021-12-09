2f30d52fb6fddfa38a66aaf55a72df1ed77c005f
soumouni <souad.mimouni13@gmail.com>
2021-12-09 14:52:28 +0100

    use if statements

---
+++ tiy_aliens.py
@@ -0,0 +1,37 @@
+# try it yourself page 84
+#alien colors
+color="yellow"
+if color=="green":  #version that passes the test
+    print("congratulations! you juste earned 5 points")
+elif color=="yellow":
+    print("you earned 10 points")
+else:
+    print("you earned 15points")
+
+#stage of life
+age=15
+if age<2 :
+    print("this person is a baby")
+elif age<4:
+    print("this person is a toddler")
+elif age<13:
+    print("this person is a kid")
+elif age<20:
+    print("this person is a teenager")
+elif age<65:
+    print("this person is an adult")
+else:
+    print("this person is an elder")
+
+#favorite fruit
+favorite_fruits=["dattes","banane","figues"]
+if "banane" in favorite_fruits:
+    print("\nyou really like banane")
+if"dattes" in favorite_fruits:
+    print("you love dattes")
+if"figues"in favorite_fruits:
+    print ('you really like figues')
+if "pasteque" in favorite_fruits:
+    print ('you really like pasteque')
+if "melon" in favorite_fruits:
+    print ('you really like melon')   
