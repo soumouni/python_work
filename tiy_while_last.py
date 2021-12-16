277207c55cfdc6d6eed87c76f9f6b93378a5ab89
soumouni <souad.mimouni13@gmail.com>
2021-12-16 14:34:40 +0100

    include test to remove all 'patrami' from the lists

--- tiy_while_last.py
+++ tiy_while_last.py
@@ -1,15 +1,21 @@
 # try it yourself page 127
 #7-8 Deli
-sandwish_orders=['novegien','mexicain', 'parisien']
+sandwish_orders=['novegien', 'pastrami','mexicain', 'parisien', 'pastrami',
+    'pastrami']
 finished_sandwiches=[]
 
+print("The Deli has run out of pastrami\n")
 while sandwish_orders:
     for sandwich in sandwish_orders:
-        print(f"I made your {sandwich} sandwich")
-        finished_sandwiches.append(sandwich)
-        sandwish_orders.remove(sandwich)
+        if sandwich == 'pastrami':
+            sandwish_orders.remove(sandwich)
+        else:
+            print(f"I made your {sandwich} sandwich")
+            finished_sandwiches.append(sandwich)
+            sandwish_orders.remove(sandwich)
 
 print("\nThe sandwiches that were made are:\n")
 for sandwich in finished_sandwiches:
     print(sandwich.title())
     
+
