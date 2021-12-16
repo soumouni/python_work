0cdcf187d45cf9c58679ada160cd98bfa11058b6
soumouni <souad.mimouni13@gmail.com>
2021-12-16 14:46:45 +0100

    use while loop to fill dictionary

--- tiy_while_last.py
+++ tiy_while_last.py
@@ -18,4 +18,16 @@ print("\nThe sandwiches that were made are:\n")
 for sandwich in finished_sandwiches:
     print(sandwich.title())
     
+#7-10 Dream vacation
+dream_vacations={}
+print("this is a poll about friend's dream vacation\n")
+while True:
+    name=input("Please enter your name ")
+    place=input("Where would be you dream vacation ")
+    dream_vacations[name]=place
+    test=input("Would you like to let another person respond? (Y/N)")
+    if test == 'N':
+        break
+for name, place in dream_vacations.items():
+    print(f"\n{name.title()} would love to visite {place.title()}")
 
