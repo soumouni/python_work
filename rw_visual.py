2bfafd134b64f4acef50d941fb9b0246d3b12416
soumouni <souad.mimouni13@gmail.com>
2022-01-22 21:30:01 +0100

    styling the plot

--- rw_visual.py
+++ rw_visual.py
@@ -1,6 +1,4 @@
 #here we plot the random walk
-from ast import Break
-from tkinter import Y
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 while True:
@@ -9,8 +7,12 @@ while True:
 
     plt.style.use('classic')
     fig, ax=plt.subplots()
-    ax.scatter(mywalk.x_values, mywalk.y_values,c='red', s=5)
+    point_number=range(mywalk.num_points)
+    ax.scatter(mywalk.x_values, mywalk.y_values,c=point_number, 
+    cmap=plt.cm.Blues, edgecolors='none', s=15)
+
     plt.show()
+    
     keep = input('do you want to continue y/n ')
     if keep == 'n':
         break
