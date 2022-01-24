dc3c1de657b1c755595a5dc662f640192d942bc7
soumouni <souad.mimouni13@gmail.com>
2022-01-24 12:39:57 +0100

    change scatter to plot

--- rw_visual.py
+++ rw_visual.py
@@ -1,18 +1,18 @@
 #here we plot the random walk
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
-while True:
-    mywalk=RandomWalk()
-    mywalk.fill_walk()
+mywalk=RandomWalk()
+mywalk.fill_walk()
 
-    plt.style.use('classic')
-    fig, ax=plt.subplots()
-    point_number=range(mywalk.num_points)
-    ax.scatter(mywalk.x_values, mywalk.y_values,c=point_number, 
-    cmap=plt.cm.Blues, edgecolors='none', s=15)
-
-    plt.show()
+plt.style.use('classic')
+fig, ax=plt.subplots(figsize=(15,10))
+point_number=range(mywalk.num_points)
     
-    keep = input('do you want to continue y/n ')
-    if keep == 'n':
-        break
+ax.plot(mywalk.x_values, mywalk.y_values, linewidth=1)
+ax.scatter(0,0,c='green', edgecolors='none', s=50)
+ax.scatter(mywalk.x_values[-1], mywalk.y_values[-1], c='red', 
+edgecolors='none', s=50)
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+plt.show()
+    
