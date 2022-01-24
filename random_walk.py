ffcdbb6f840a44ae7f421a22f5f1714ff3534e95
soumouni <souad.mimouni13@gmail.com>
2022-01-24 12:38:47 +0100

    define get step function, update fill walk

--- random_walk.py
+++ random_walk.py
@@ -7,17 +7,20 @@ class RandomWalk :
         #All walks starts at (0,0)
         self.x_values=[0]
         self.y_values=[0]
+
+    def get_step(self):
+        """this determine the next step"""
+        direction=choice([-1,1])
+        distance=choice([0,1,2,3,4])
+        movement=direction*distance
+        return movement
+
     def fill_walk(self):
         """calculate all the points in the walk"""
         #keep taking steps until the walk reaches the desired length
         while len(self.x_values)<self.num_points:
-            x_direction=choice([-1,1])
-            x_distance=choice([0,1,2,3,4])
-            x_step=x_direction * x_distance
-
-            y_direction=choice([-1,1])
-            y_distance=choice([0,1,2,3,4])
-            y_step=y_direction * y_distance
+            x_step=self.get_step()
+            y_step=self.get_step()            
 
             if x_step == 0 and y_step == 0:
                 continue
@@ -27,3 +30,4 @@ class RandomWalk :
 
             self.x_values.append(x)
             self.y_values.append(y)
+
