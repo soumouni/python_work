d031152235618c858a7762029d33a03e947363aa
Souad Mimouni <94570112+soumouni@users.noreply.github.com>
2022-01-25 22:12:43 +0100

    use list comprehension for results

--- dice_visual.py
+++ dice_visual.py
@@ -1,19 +1,21 @@
 from plotly.graph_objs import Bar, Layout
 from plotly import offline
 from die import Die
-# create one D6 and one D10 dice
+# create two D6 dice
 die_1 = Die()
-die_2=Die(10)
+die_2=Die()
 
 #make some rolls and store the results in a list.
-results=[]
-for roll_num in range(50_000):
-    result=die_1.roll() +die_2.roll()
-    results.append(result)
+#results=[]
+#for roll_num in range(100_000):
+#    result=die_1.roll() * die_2.roll()
+#    results.append(result)
+
+results=[die_1.roll() + die_2.roll() for num in range(100_000)]
 
 #Analyze results
 frequencies=[]
-max_result=die_1.num_sides + die_2.num_sides
+max_result=die_1.num_sides + die_2.num_sides 
 for value in range(2, max_result+1):
     frequency=results.count(value)
     frequencies.append(frequency)
@@ -23,6 +25,6 @@ x_values=list(range(2,max_result+1))
 data=[Bar(x=x_values, y=frequencies)]
 x_axis_config={'title':'Result','dtick':1}
 y_axis_config={'title':'Frequency of results'}
-my_layout=Layout(title='Results of rolling a D6 and a D10, 50 000 times', 
+my_layout=Layout(title='Results of rolling two D6, 100 000 times', 
     xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data':data, 'layout':my_layout},filename='d6_d10.html')
+offline.plot({'data':data, 'layout':my_layout},filename='d6_d6.html')
